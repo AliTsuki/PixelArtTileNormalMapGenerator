46e06ebe21e1256ed01cdd4673947a6c6e465056
Ali M <PhotonPhighter@gmail.com>
2019-11-26 19:56:29 -0700

    0.0.1a
    Initial upload

---
+++ NormalMapGeneratorForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace PixelArtTileNormalMapGenerator
+{
+    partial class NormalMapGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OriginalImagePreviewBox = new System.Windows.Forms.PictureBox();
+            this.NormalMapImagePreviewBox = new System.Windows.Forms.PictureBox();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.IgnoreColorDialog = new System.Windows.Forms.ColorDialog();
+            this.GenerateNormalMapButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.IgnoreColorDialogButton = new System.Windows.Forms.Button();
+            this.BackgroundColorDialog = new System.Windows.Forms.ColorDialog();
+            this.SeparatorColorDialog = new System.Windows.Forms.ColorDialog();
+            this.IndividualColorDialog = new System.Windows.Forms.ColorDialog();
+            this.SelectBackgroundColorButton = new System.Windows.Forms.Button();
+            this.SelectSeparatorColorButton = new System.Windows.Forms.Button();
+            this.SelectIndividualColorButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.IgnoreColorBox = new System.Windows.Forms.PictureBox();
+            this.BackgroundColorBox = new System.Windows.Forms.PictureBox();
+            this.SeparatorColorBox = new System.Windows.Forms.PictureBox();
+            this.IndividualColorBox = new System.Windows.Forms.PictureBox();
+            this.OriginalImageLabel = new System.Windows.Forms.Label();
+            this.NormalMapImageLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImagePreviewBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NormalMapImagePreviewBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IgnoreColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SeparatorColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IndividualColorBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OriginalImagePreviewBox
+            // 
+            this.OriginalImagePreviewBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OriginalImagePreviewBox.Location = new System.Drawing.Point(207, 47);
+            this.OriginalImagePreviewBox.Name = "OriginalImagePreviewBox";
+            this.OriginalImagePreviewBox.Size = new System.Drawing.Size(512, 512);
+            this.OriginalImagePreviewBox.TabIndex = 0;
+            this.OriginalImagePreviewBox.TabStop = false;
+            // 
+            // NormalMapImagePreviewBox
+            // 
+            this.NormalMapImagePreviewBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NormalMapImagePreviewBox.Location = new System.Drawing.Point(745, 47);
+            this.NormalMapImagePreviewBox.Name = "NormalMapImagePreviewBox";
+            this.NormalMapImagePreviewBox.Size = new System.Drawing.Size(512, 512);
+            this.NormalMapImagePreviewBox.TabIndex = 1;
+            this.NormalMapImagePreviewBox.TabStop = false;
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(25, 47);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(128, 23);
+            this.LoadImageButton.TabIndex = 2;
+            this.LoadImageButton.Text = "Load Image";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // GenerateNormalMapButton
+            // 
+            this.GenerateNormalMapButton.Location = new System.Drawing.Point(25, 383);
+            this.GenerateNormalMapButton.Name = "GenerateNormalMapButton";
+            this.GenerateNormalMapButton.Size = new System.Drawing.Size(128, 23);
+            this.GenerateNormalMapButton.TabIndex = 3;
+            this.GenerateNormalMapButton.Text = "Generate Normal Map";
+            this.GenerateNormalMapButton.UseVisualStyleBackColor = true;
+            this.GenerateNormalMapButton.Click += new System.EventHandler(this.GenerateNormalMapButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(50, 429);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 4;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // IgnoreColorDialogButton
+            // 
+            this.IgnoreColorDialogButton.Location = new System.Drawing.Point(24, 120);
+            this.IgnoreColorDialogButton.Name = "IgnoreColorDialogButton";
+            this.IgnoreColorDialogButton.Size = new System.Drawing.Size(129, 25);
+            this.IgnoreColorDialogButton.TabIndex = 5;
+            this.IgnoreColorDialogButton.Text = "Select Ignore Color";
+            this.IgnoreColorDialogButton.UseVisualStyleBackColor = true;
+            this.IgnoreColorDialogButton.Click += new System.EventHandler(this.IgnoreColorDialogButton_Click);
+            // 
+            // SelectBackgroundColorButton
+            // 
+            this.SelectBackgroundColorButton.Location = new System.Drawing.Point(24, 152);
+            this.SelectBackgroundColorButton.Name = "SelectBackgroundColorButton";
+            this.SelectBackgroundColorButton.Size = new System.Drawing.Size(129, 23);
+            this.SelectBackgroundColorButton.TabIndex = 6;
+            this.SelectBackgroundColorButton.Text = "Select Background Color";
+            this.SelectBackgroundColorButton.UseVisualStyleBackColor = true;
+            this.SelectBackgroundColorButton.Click += new System.EventHandler(this.SelectBackgroundColorButton_Click);
+            // 
+            // SelectSeparatorColorButton
+            // 
+            this.SelectSeparatorColorButton.Location = new System.Drawing.Point(24, 182);
+            this.SelectSeparatorColorButton.Name = "SelectSeparatorColorButton";
+            this.SelectSeparatorColorButton.Size = new System.Drawing.Size(129, 23);
+            this.SelectSeparatorColorButton.TabIndex = 7;
+            this.SelectSeparatorColorButton.Text = "Select Separator Color";
+            this.SelectSeparatorColorButton.UseVisualStyleBackColor = true;
+            this.SelectSeparatorColorButton.Click += new System.EventHandler(this.SelectSeparatorColorButton_Click);
+            // 
+            // SelectIndividualColorButton
+            // 
+            this.SelectIndividualColorButton.Location = new System.Drawing.Point(24, 212);
+            this.SelectIndividualColorButton.Name = "SelectIndividualColorButton";
+            this.SelectIndividualColorButton.Size = new System.Drawing.Size(129, 23);
+            this.SelectIndividualColorButton.TabIndex = 8;
+            this.SelectIndividualColorButton.Text = "Select Individual Color";
+            this.SelectIndividualColorButton.UseVisualStyleBackColor = true;
+            this.SelectIndividualColorButton.Click += new System.EventHandler(this.SelectIndividualColorButton_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            // 
+            // IgnoreColorBox
+            // 
+            this.IgnoreColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IgnoreColorBox.Location = new System.Drawing.Point(160, 120);
+            this.IgnoreColorBox.Name = "IgnoreColorBox";
+            this.IgnoreColorBox.Size = new System.Drawing.Size(25, 25);
+            this.IgnoreColorBox.TabIndex = 9;
+            this.IgnoreColorBox.TabStop = false;
+            // 
+            // BackgroundColorBox
+            // 
+            this.BackgroundColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BackgroundColorBox.Location = new System.Drawing.Point(160, 152);
+            this.BackgroundColorBox.Name = "BackgroundColorBox";
+            this.BackgroundColorBox.Size = new System.Drawing.Size(25, 23);
+            this.BackgroundColorBox.TabIndex = 10;
+            this.BackgroundColorBox.TabStop = false;
+            // 
+            // SeparatorColorBox
+            // 
+            this.SeparatorColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SeparatorColorBox.Location = new System.Drawing.Point(160, 182);
+            this.SeparatorColorBox.Name = "SeparatorColorBox";
+            this.SeparatorColorBox.Size = new System.Drawing.Size(25, 22);
+            this.SeparatorColorBox.TabIndex = 11;
+            this.SeparatorColorBox.TabStop = false;
+            // 
+            // IndividualColorBox
+            // 
+            this.IndividualColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IndividualColorBox.Location = new System.Drawing.Point(160, 212);
+            this.IndividualColorBox.Name = "IndividualColorBox";
+            this.IndividualColorBox.Size = new System.Drawing.Size(25, 23);
+            this.IndividualColorBox.TabIndex = 12;
+            this.IndividualColorBox.TabStop = false;
+            // 
+            // OriginalImageLabel
+            // 
+            this.OriginalImageLabel.AutoSize = true;
+            this.OriginalImageLabel.Location = new System.Drawing.Point(426, 28);
+            this.OriginalImageLabel.Name = "OriginalImageLabel";
+            this.OriginalImageLabel.Size = new System.Drawing.Size(74, 13);
+            this.OriginalImageLabel.TabIndex = 13;
+            this.OriginalImageLabel.Text = "Original Image";
+            // 
+            // NormalMapImageLabel
+            // 
+            this.NormalMapImageLabel.AutoSize = true;
+            this.NormalMapImageLabel.Location = new System.Drawing.Point(963, 28);
+            this.NormalMapImageLabel.Name = "NormalMapImageLabel";
+            this.NormalMapImageLabel.Size = new System.Drawing.Size(64, 13);
+            this.NormalMapImageLabel.TabIndex = 14;
+            this.NormalMapImageLabel.Text = "Normal Map";
+            // 
+            // NormalMapGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 606);
+            this.Controls.Add(this.NormalMapImageLabel);
+            this.Controls.Add(this.OriginalImageLabel);
+            this.Controls.Add(this.IndividualColorBox);
+            this.Controls.Add(this.SeparatorColorBox);
+            this.Controls.Add(this.BackgroundColorBox);
+            this.Controls.Add(this.IgnoreColorBox);
+            this.Controls.Add(this.SelectIndividualColorButton);
+            this.Controls.Add(this.SelectSeparatorColorButton);
+            this.Controls.Add(this.SelectBackgroundColorButton);
+            this.Controls.Add(this.IgnoreColorDialogButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.GenerateNormalMapButton);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.NormalMapImagePreviewBox);
+            this.Controls.Add(this.OriginalImagePreviewBox);
+            this.Name = "NormalMapGeneratorForm";
+            this.Text = "Pixel Art Tile Normal Map Generator";
+            this.Load += new System.EventHandler(this.NormalMapGeneratorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImagePreviewBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NormalMapImagePreviewBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IgnoreColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SeparatorColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IndividualColorBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox OriginalImagePreviewBox;
+        private System.Windows.Forms.PictureBox NormalMapImagePreviewBox;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.ColorDialog IgnoreColorDialog;
+        private System.Windows.Forms.Button GenerateNormalMapButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button IgnoreColorDialogButton;
+        private System.Windows.Forms.ColorDialog BackgroundColorDialog;
+        private System.Windows.Forms.ColorDialog SeparatorColorDialog;
+        private System.Windows.Forms.ColorDialog IndividualColorDialog;
+        private System.Windows.Forms.Button SelectBackgroundColorButton;
+        private System.Windows.Forms.Button SelectSeparatorColorButton;
+        private System.Windows.Forms.Button SelectIndividualColorButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.PictureBox IgnoreColorBox;
+        private System.Windows.Forms.PictureBox BackgroundColorBox;
+        private System.Windows.Forms.PictureBox SeparatorColorBox;
+        private System.Windows.Forms.PictureBox IndividualColorBox;
+        private System.Windows.Forms.Label OriginalImageLabel;
+        private System.Windows.Forms.Label NormalMapImageLabel;
+    }
+}
+
---
+++ NormalMapGeneratorForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelArtTileNormalMapGenerator
+{
+    public partial class NormalMapGeneratorForm : Form
+    {
+        private static string DefaultNormalMapImageDirectoryPath = Application.StartupPath + "\\DefaultNormal\\";
+        private static string DefaultNormalMapImageName = "DefaultNormal.png";
+        private static Bitmap OriginalImageBitmap;
+        private static Bitmap NormalMapDefaultImageBitmap;
+        private static Bitmap NormalMapImageBitmap;
+        private static Color NormalMapBackgroundDefaultColor;
+        private static Color IgnoreColor;
+        private static Color BackgroundColor;
+        private static Color SeparatorColor;
+        private static Color IndividualColor;
+
+        public NormalMapGeneratorForm()
+        {
+            this.InitializeComponent();
+        }
+
+        // Set default values
+        // On Load Image button clicked select image from disk
+        // Load image into memory
+        // Show image in preview box
+        // Save selected colors, Ignore, background, separator, individual
+        // On Generate Normal Map button clicked, generate new image, same size as original, go through each pixel in image
+        // -ignore ignore colors, turn background colors into normal bluish color, on separator color create new tile object
+        // -store pixel location, do recursive flood fill, check against pixels already added, check all nearby pixels
+        // -if they are individual color add them to object, if separator color, add to object in separate list and don't seek farther
+        // -in that direction. Upon completion of flood fill, add separator list to individual list, get width and height of individual tile object
+        // -get scale difference between object and normal map default texture provided, for each pixel in object, translate to pixel location in
+        // -normal map default, use scale also to determin how many pixels would be averaged in shrinking, grab all pixels around selected in normal default,
+        // -average them to get final value to be assigned to pixel of object, loop through all objects like that, once all objects are instantiated and have
+        // - their colors selected, go through new blank normal map image and place all the pixels in place
+        // Show normal map in preview box
+        // On Save Normal Map button clicked, save normal map image as png in same path as original image
+        // On Reset button clicked, clear all saved colors and images
+        private void NormalMapGeneratorForm_Load(object sender, EventArgs args)
+        {
+            this.InitializeValues();
+        }
+
+        private void InitializeValues()
+        {
+            LoadDefaultNormal();
+            OriginalImageBitmap = null;
+            NormalMapImageBitmap = null;
+            IgnoreColor = Color.White;
+            BackgroundColor = Color.White;
+            SeparatorColor = Color.White;
+            IndividualColor = Color.White;
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs args)
+        {
+            DialogResult result = this.OpenFileDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                try
+                {
+                    OriginalImageBitmap = new Bitmap(this.OpenFileDialog.FileName);
+                    this.OriginalImagePreviewBox.Image = OriginalImageBitmap;
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show($@"Unable to read image!{Environment.NewLine}Please select a valid image format.{Environment.NewLine}For best results use PNG format.");
+                }
+            }
+        }
+
+        private void IgnoreColorDialogButton_Click(object sender, EventArgs args)
+        {
+            DialogResult result = this.IgnoreColorDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                IgnoreColor = this.IgnoreColorDialog.Color;
+                this.IgnoreColorBox.BackColor = IgnoreColor;
+            }
+        }
+
+        private void SelectBackgroundColorButton_Click(object sender, EventArgs args)
+        {
+            this.BackgroundColorDialog.ShowDialog();
+            BackgroundColor = this.BackgroundColorDialog.Color;
+            this.BackgroundColorBox.BackColor = BackgroundColor;
+        }
+
+        private void SelectSeparatorColorButton_Click(object sender, EventArgs args)
+        {
+            this.SeparatorColorDialog.ShowDialog();
+            SeparatorColor = this.SeparatorColorDialog.Color;
+            this.SeparatorColorBox.BackColor = SeparatorColor;
+        }
+
+        private void SelectIndividualColorButton_Click(object sender, EventArgs args)
+        {
+            this.IndividualColorDialog.ShowDialog();
+            IndividualColor = this.IndividualColorDialog.Color;
+            this.IndividualColorBox.BackColor = IndividualColor;
+        }
+
+        private void GenerateNormalMapButton_Click(object sender, EventArgs args)
+        {
+            if(OriginalImageBitmap != null)
+            {
+                NormalMapImageBitmap = new Bitmap(OriginalImageBitmap.Width, OriginalImageBitmap.Height);
+                this.CreateNormalMap();
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs args)
+        {
+            this.InitializeValues();
+        }
+
+        private void LoadDefaultNormal()
+        {
+            try
+            {
+                if(Directory.Exists(DefaultNormalMapImageDirectoryPath))
+                {
+                    NormalMapDefaultImageBitmap = new Bitmap(DefaultNormalMapImageDirectoryPath + DefaultNormalMapImageName);
+                }
+                else
+                {
+                    Directory.CreateDirectory(DefaultNormalMapImageDirectoryPath);
+                    throw new Exception("Default Normal Map Image Directory did not exist! Creating Directory!");
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show($@"Error reading default normal map!{Environment.NewLine}Has the image been moved or deleted?{Environment.NewLine}Please restore the default normal map image to \DefaultNormal\DefaultNormal.PNG and press the RESET button to continue.");
+            }
+        }
+
+        private void CreateNormalMap()
+        {
+            try
+            {
+                for(int x = 0; x < OriginalImageBitmap.Width; x++)
+                {
+                    for(int y = 0; y < OriginalImageBitmap.Height; y++)
+                    {
+                        Color currentPixelColor = OriginalImageBitmap.GetPixel(x, y);
+                        if(currentPixelColor == IgnoreColor)
+                        {
+                            break;
+                        }
+                        else if(currentPixelColor == BackgroundColor)
+                        {
+                            NormalMapImageBitmap.SetPixel(x, y, NormalMapBackgroundDefaultColor);
+                        }
+                        else if(currentPixelColor == SeparatorColor)
+                        {
+
+                        }
+                        else if(currentPixelColor == IndividualColor)
+                        {
+
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show($@"Error creating Normal Map!{Environment.NewLine}{e.ToString()}");
+            }
+        }
+
+        private int GetColorDistance(Color currentColor, Color colorToCheckAgainst)
+        {
+            return 0;
+        }
+    }
+
+    public class TileObject
+    {
+
+    }
+}
